b4470917e57cd4ad49e604aa752b6f5fae78bfe9
Esad <oesadozturk@gmail.com>
2026-02-07 07:19:43 +0300

    Bug fix and add validation

--- digit_recognition.py
+++ digit_recognition.py
@@ -7,8 +7,8 @@ from keras.datasets import mnist
 
 train_data = train_data.reshape(60000, 28*28)
 train_data = train_data.astype("float32") / 255
-train_labels = train_labels.reshape(60000, 28*28)
-train_labels = train_labels.astype("float32") / 255
+test_data = test_data.reshape(10000, 28*28)
+test_data = test_data.astype("float32") / 255
 
 model = keras.Sequential([
     layers.Dense(64, activation="relu"),
@@ -20,4 +20,4 @@ model.compile(optimizer="rmsprop",
               loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
 
-model.fit(train_data, train_labels, epochs=7, batch_size=128)
+model.fit(train_data, train_labels, epochs=8, batch_size=128, validation_split=0.2)
